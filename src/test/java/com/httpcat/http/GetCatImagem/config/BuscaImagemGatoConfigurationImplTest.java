5e0d1604f5761bb112c4d312a6c60e30d92d253e
andre.bluiz94@gmail.com <934640698andre>
2020-11-13 20:39:29 -0300

    Ajustes dos teste falhando

--- src/test/java/com/httpcat/http/GetCatImagem/config/BuscaImagemGatoConfigurationImplTest.java
+++ src/test/java/com/httpcat/http/GetCatImagem/config/BuscaImagemGatoConfigurationImplTest.java
@@ -43,7 +43,6 @@ public class BuscaImagemGatoConfigurationImplTest {
 		String urlExpect = getUrlExpect(expand);
 
 		assertEquals(urlExpect, url);
-
 	}
 
 	private String getUrlExpect(Map<String, String> expand) {
--- src/test/java/com/httpcat/http/raca/config/BuscarRacasGatoConfigurationImplTest.java
+++ src/test/java/com/httpcat/http/raca/config/BuscarRacasGatoConfigurationImplTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest(classes = {
 		BuscarRacasGatoConfigurationImpl.class
 })
+@ActiveProfiles("test")
 public class BuscarRacasGatoConfigurationImplTest {
 
 	@Value("${cat.url.racas.dominio}")
